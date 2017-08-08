02bd1dcda1e177574919ff1077ffb370b544f1db
sanlouise <sandra.adamshallie@gmail.com>
2017-08-08 13:46:08 -0400

    Add yargs

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+//Object that stores the final parsed output
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
 
 const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=Via%20della%20Lungara,%20233,%2000165%20Roma%20RM,%20Italy'
 request({
-  url
+  url,
+  json: true,
 }, (error, response, body) => {
-   console.log(body);
-})
+   console.log(`Address: ${body.results[0].formatted_address}`);
+   console.log(`Latitude:
+   ${body.results[0].geometry.location.lat}`);
+   console.log(`Longitude:
+   ${body.results[0].geometry.location.lng}`);
+});
